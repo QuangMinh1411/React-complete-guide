561c2b7cb578d19c7b0e8c35fe56f237657f117c
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2022-09-26 16:12:27 +0700

    update char bar

--- basic component/react-complete-guide/src/App.js
+++ basic component/react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import Expense from './components/Expenses/Expense';
+import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 function App() {
   const INTIALEXPENSES = [
@@ -35,7 +35,7 @@ function App() {
   return (
     <div>
       <NewExpense addExpense={addExpense} />
-      <Expense items = {expenses}/>
+      <Expenses items = {expenses}/>
     </div> 
   );
 }
--- basic component/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ basic component/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 export default function NewExpense(props) {
@@ -8,11 +8,20 @@ export default function NewExpense(props) {
       id: Math.random().toString()
     }
     props.addExpense(expenseData);
+    setEdit(false)
   }
-  
+  const [isEdit,setEdit]=useState(false);
+  const editHandler = ()=>{
+    setEdit(true);
+  }
+  const stopEditHandler = ()=>{
+    setEdit(false)
+  }
+
   return (
     <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler} />
+        {!isEdit&&<button onClick={editHandler}>Add New Expense</button>}
+        {isEdit&&<ExpenseForm onSaveExpenseData ={saveExpenseDataHandler} onCancel={stopEditHandler} />}
     </div>
   )
 }
