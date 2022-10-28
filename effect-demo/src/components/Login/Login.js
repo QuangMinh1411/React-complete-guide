7d3438f4e355077c8485d329dfcaf02a9e907ba3
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2022-10-28 08:50:56 +0700

    Use Context

--- effect-demo/src/components/Login/Login.js
+++ effect-demo/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -98,8 +99,10 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogIn(emailState.value, passwordState.value);
   };
+
+  const authCtx = useContext(AuthContext);
 
   return (
     <Card className={classes.login}>
