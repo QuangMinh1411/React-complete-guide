65985cb6bf306ce0f9ef2b093db3a7058ad7b9c4
Quang <tomminhquan1411@gmail.com>
2022-12-14 12:54:19 +0700

    Item CRUD

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
   const [cartShown, setCartShown] = useState(false);
   const showCartHandler = () => {
@@ -11,13 +12,13 @@ function App() {
     setCartShown(false);
   };
   return (
-    <Fragment>
+    <CartProvider>
       {cartShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
